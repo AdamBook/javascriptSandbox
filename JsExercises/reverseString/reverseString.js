a85c3a89bd90af17e392eda5395a74fc9393fef9
Adam Book <cAdamBook@gmail.com>
2021-04-07 11:04:27 +0200

    First attempt on reverseString exercise.

--- JsExercises/reverseString/reverseString.js
+++ JsExercises/reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+    let forwardString = word;
+    let reversedString = '';
+    let slicedString = forwardString;
+
+    for (i=1; i<forwardString.length(); i++){
+
+        reversedString += forwardString.substr(-1);
+        forwardString = forwardString.slice(0,-1);
+
+    }
+
+    return reversedString;
 
 }
 
