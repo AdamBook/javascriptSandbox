a4c3aaac3a9a49765c1791f7cf2a6c58c741c9bf
Adam Book <cAdamBook@gmail.com>
2021-04-08 12:40:06 +0200

    First attempt at removeFromArray test 1.

--- JsExercises/removeFromArray/removeFromArray.js
+++ JsExercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
 const removeFromArray = function() {
 
+
+        let copyOfArgs = [];
+        let returningArgs = []
+
+        for (i=0; i<arguments.length; i++){
+
+            copyOfArgs[i] = arguments[i];
+
+        }
+    
+
+        for (i=0; i<copyOfArgs.length; i++){
+
+            for (b=0; b<arguments.length; b++){
+
+                if (copyOfArgs[i] == arguments[b]){
+                    returningArgs += arguments[b];
+                }
+            }
+
+        }
+
+        return returningArgs;
+
 }
 
 module.exports = removeFromArray
