90e5942087437a41a5d2bbe23b837cd6c4318226
Adam Book <cAdamBook@gmail.com>
2021-04-11 12:45:59 +0200

    Solved leapYears with all tests.

--- jsExercises/leapYears/leapYears.js
+++ jsExercises/leapYears/leapYears.js
@@ -1,21 +1,21 @@
 const leapYears = function (year) {
 
-    if (year % 4 == 0 && !year % 100 == 0) {
-        if (year % 400 == 0) {
+    if (year % 4 == 0) {
+
+        if (year %100 ==0 && year%400 == 0){
             return true;
         }
-        else {
-            //return false;
-            return 'First';
+        if (year %100==0 && year%400 != 0){
+            return false;
         }
-
+      
         return true;
-    }
-    else {
-        
-        //return false;
-        return 'Second';
 
+
+    }
+
+    else{
+        return false;
     }
 
 }
